66a93515de3ad5a7082a6053386d854260b88578
transformers <mahmud.mahbub@tekschool.us>
2023-02-18 21:29:13 -0500

    Unit test class added

---
+++ src/test/UnitTest.java
@@ -0,0 +1,16 @@
+package test;
+
+public class UnitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 15;
+		int c = a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
