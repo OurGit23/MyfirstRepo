7edfd606d0c4210238e53bf28ada26c66bafc8d7
OurGit23 <125595551+OurGit23@users.noreply.github.com>
2023-02-21 20:08:19 -0500

    Update GitPracticeTwo.java

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -9,6 +9,10 @@ public class GitPracticeTwo {
 		int y = 20;
 		int z = y - x;
 		System.out.println(z);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println("this is for pull practice");	
+		
 	}
 
 }
